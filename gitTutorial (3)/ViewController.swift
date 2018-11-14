0220f87540269cfa6f22c22783f18324edac26a5
qualan <qualan.tb@gmail.com>
2018-11-14 12:06:11 -0600

    this is the og nothing new here so yea

---
+++ gitTutorial (3)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTutorial (3)
+//
+//  Created by Bradford, Qualan T on 11/14/18.
+//  Copyright © 2018 Bradford, Qualan T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
