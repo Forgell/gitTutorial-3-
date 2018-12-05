17fa707a3c5d827bbfd697d4fea8d6fe256e10f8
qualan <qualan.tb@gmail.com>
2018-12-05 11:12:23 -0600

    there is a print statmet in view controller I added

--- gitTutorial (3)/ViewController.swift
+++ gitTutorial (3)/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: UIButton) {
         print("This is a Git tutorial")
+        print("youve finished")
     }
 
+    
 }
 
